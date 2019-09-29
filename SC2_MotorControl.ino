814542f15e60ed7564176724d332d250259db643
Timmyshasta <40968570+Timmyshasta@users.noreply.github.com>
2019-09-28 19:14:15 -0700

    Add files via upload

--- SC2_MotorControl.ino
+++ SC2_MotorControl.ino
@@ -1,15 +1,9 @@
-
-
-
 // Jrk motor controller library
 // Test, calibrate, and configure motors & controllers via USB using Pololu software
 // Refer to reference user guide for configuring motors
 // Baud rate must be set to 9600 && CRC should be DISABLED
-
 #include <JrkG2.h> 
 #include "BluetoothSerial.h"
-
-
 
 //Jrk Driver side motor controller
 //int driver = 11;
@@ -27,19 +21,26 @@ void setup() {
 }
 
 void loop() {
+  
   if(Serial.available() >0) {
-    String acd = Serial.readString();
-    String acp = Serial.readString();
-    String aod = Serial.readString();
-    String aop = Serial.readString();
-    if(acd = "Auto close driver"){
-      autoclose_driver();
+    String cmd = Serial.readString();
+
+    if(cmd == "Auto close driver\n"){
+      autoclose_driver();   
+    }
+    else if(cmd == "Auto close passenger\n") {
+      autoclose_passenger();
+    }
+    else if(cmd == "Auto open driver\n"){
+      autoopen_driver();
+    }
+    else if(cmd == "Auto open passenger\n"){
+      autoopen_passenger();
     }
     else {
-      Serial.print("what");
-    }
+      Serial.println("Please input valid command");
+    }   
   }
-
 }
 
 void autoclose_driver(){
@@ -60,7 +61,7 @@ void autoopen_driver(){
   delay(1000);
 }
 
-void autoopen_driver(){
+void autoopen_passenger(){
   delay(1000);
   jrk1.setTarget(4095);
   delay(1000);
