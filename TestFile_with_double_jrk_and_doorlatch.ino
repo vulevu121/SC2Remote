9c8f64543d6e3dcbe19eac89e88fd667cd685685
Timmyshasta <40968570+Timmyshasta@users.noreply.github.com>
2019-10-08 19:18:43 -0700

    With door latch feedback logic

--- TestFile_with_double_jrk_and_doorlatch.ino
+++ TestFile_with_double_jrk_and_doorlatch.ino
@@ -16,20 +16,24 @@ const int DriverSwitch = 34; //Driver Door internal/external switch GPIO to ESP3
 const int PassengerSwitch = 39; //Passenger Door internal/external switch GPIO to ESP32 board
 const int DriverDoorLatch = 21; //Driver door actuator control, Pull Low to engage
 const int PassengerDoorLatch = 14; //Passenger door actuator control, Pull Low to engage
+const int DriverLatchFeedback = 33; //Driver door latch feedback status
+const int PassengerLatchFeedback = 27; //Passenger door latch feedback status
 String inData; //String variable name to take input from SerialBT command
 enum ascii {K = 75, O = 79}; // ASCII format for unsigned int for SerialBT.write 
 
 void setup() {
   Serial.begin(9600); //Initialize baud rate for serial comm
+  jrkSerial.begin(9600); //Initialize baud rate for bluetooth serial comm
   SerialBT.begin("ESP32test"); //Bluetooth device name
   Serial.println("The device started, now you can pair it with bluetooth!");
   pinMode(DriverSwitch, INPUT); //Define driver switch as an input
   pinMode(PassengerSwitch, INPUT); //Define passenger switch as an input
   pinMode(DriverDoorLatch, OUTPUT); //Define driver door latch as an ouput
   pinMode(PassengerDoorLatch, OUTPUT); //Define passenger door latch as an output
+  pinMode(DriverLatchFeedback, INPUT); //Define driver door latch FEEDBACK as an input
+  pinMode(PassengerLatchFeedback, INPUT); //Define passenger door latch FEEDBACK as an input
   digitalWrite(DriverDoorLatch,HIGH); //Initial State to "High" relay "OFF"
   digitalWrite(PassengerDoorLatch,HIGH);  //Initial State to "High" relay "OFF"
-  jrkSerial.begin(9600); //Initialize baud rate for bluetooth serial comm
 }
 
 void loop() {
@@ -37,70 +41,187 @@ void loop() {
   {
     char received = SerialBT.read(); //SerialBT receives one char
     inData += received; // all receive char will be parsed as an input
-
     if (received == '\n') //if received data returns new line ending, compare input for proper command functions
     {
       Serial.println(inData); //Print the received command
       
+//-----------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------Conditions when certain commands are met -----------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------
+           
+      //-----------Auto close driver door control -----------------------------------------------------------------------------
+     
+      
+      //Add check for door latch? 
+      
       if(inData == "Auto close driver\n"){
         autoclose_driver();   
       }
+
+      
+      //-----------Auto close passenger door control --------------------------------------------------------------------------
+    
+      //Add check for door latch?
+      
       else if(inData == "Auto close passenger\n") {
         autoclose_passenger();
       }
-      else if(inData == "Auto open driver\n"){
-        driver_door_latch_open();
-        delay(1000);
-        autoopen_driver();
-        delay(3000);
-        driver_door_latch_close();
 
+      
+      //-----------Auto open driver door control-------------------------------------------------------------------------------
+      else if(inData == "Auto open driver\n")
+      {
+        driver_door_latch_open();
+        delay(2000);
+        int driver_latch_feedbackstate = digitalRead(DriverLatchFeedback);
+        
+          if(driver_latch_feedbackstate == HIGH)
+          {
+            Serial.println("OK to open, driver unlatched detected");
+            autoopen_driver();
+            delay(3000);
+            driver_door_latch_close();
+            delay(1000);
+          }
+          else
+          {
+            Serial.println("Latch problem, please check the systems");
+            on_release_driver();
+            
+            driver_door_latch_close();
+            delay(2000);
+          }
       }
-      else if(inData == "Auto open passenger\n"){
+      //----------Auto open passenger door control-----------------------------------------------------------------------------
+      else if(inData == "Auto open passenger\n")
+      {
         passenger_door_latch_open();
-        delay(1000);
-        autoopen_passenger();
-        delay(3000);
-        passenger_door_latch_close();
+        delay(2000);
+        int passenger_latch_feedbackstate = digitalRead(PassengerLatchFeedback);
+        
+          if(passenger_latch_feedbackstate == HIGH){
+            Serial.println("OK to open, passenger unlatched detected");
+            autoopen_passenger();
+            delay(3000);
+            passenger_door_latch_close();
+            delay(1000);
+          }
+         else
+         {
+          Serial.println("Latch problem, please check the system");
+          on_release_passenger();
+          passenger_door_latch_close();
+          delay(2000);
+         }
       }
-      else if(inData == "Auto close both\n"){
+      //--------Auto close both door control-----------------------------------------------------------------------------------
+      
+      else if(inData == "Auto close both\n")
+      {
         autoclose_both();
       }
-      else if(inData == "Auto open both\n"){
+      
+      //-------Auto open both door control-------------------------------------------------------------------------------------
+      else if(inData == "Auto open both\n")
+      {
         driver_door_latch_open();
         passenger_door_latch_open();
-        delay(1000);
-        autoopen_both();
-        delay(3000);
-        driver_door_latch_close();
-        passenger_door_latch_close();
+        delay(2500);
+        int driver_latch_feedbackstate = digitalRead(DriverLatchFeedback);
+        int passenger_latch_feedbackstate = digitalRead(PassengerLatchFeedback);
+
+          if(driver_latch_feedbackstate == HIGH && passenger_latch_feedbackstate == HIGH)
+          {
+            Serial.println("OK to open, both doors unlatched detected");
+            autoopen_both();
+            delay(3000);
+            passenger_door_latch_close();
+            driver_door_latch_close();
+            delay(1000);
+          }
+          else
+          {
+            Serial.println("Latch problem, please check the system");
+            on_release_passenger();
+            on_release_driver();
+            driver_door_latch_close();
+            passenger_door_latch_close();
+            delay(2000);
+          }    
       }
-      else if(inData == "Manual driver open\n"){
+      
+      //------Manual open driver door control----------------------------------------------------------------------------------
+      else if(inData == "Manual driver open\n")
+      {
         driver_door_latch_open();
-        delay(1000);
-        on_press_driver_open();
-        delay(3000);
-        driver_door_latch_close();
+        delay(2000);
+        int driver_latch_feedbackstate = digitalRead(DriverLatchFeedback);
+
+          if(driver_latch_feedbackstate == HIGH)
+          {
+            Serial.println("OK to open, both doors unlatched detected");
+            on_press_driver_open();
+            delay(3000);
+            driver_door_latch_close();
+            delay(1000);
+          }
+          else
+          {
+            Serial.println("Latch problem, please check the system");
+            on_release_driver();
+            driver_door_latch_close();
+            delay(2000);
+          }
       }
+
+      //-----Manual close driver door control----------------------------------------------------------------------------------
       else if(inData == "Manual driver close\n"){
         on_press_driver_close();
       }
+
+      //-----Manual driver door stop control-----------------------------------------------------------------------------------
       else if (inData == "Manual driver stop\n"){
         on_release_driver();
       }
-      else if (inData == "Manual passenger open\n"){
-        driver_door_latch_open();
-        delay(1000);
-        on_press_passenger_open();
-        delay(3000);
-        driver_door_latch_close();
-      }
+
+      //-----Manual open passenger door control--------------------------------------------------------------------------------
+      
+      
+      else if (inData == "Manual passenger open\n")
+      {
+        passenger_door_latch_open();
+        delay(2000);
+        int passenger_latch_feedbackstate = digitalRead(PassengerLatchFeedback);
+
+          if(passenger_latch_feedbackstate == HIGH)
+          {
+            Serial.println("OK to open, both doors unlatched detected");
+            on_press_passenger_open();
+            delay(3000);
+            passenger_door_latch_close();
+            delay(1000);
+          }
+          else
+          {
+            Serial.println("Latch problem, please check the system");
+            on_release_passenger();
+            passenger_door_latch_close();
+            delay(2000);
+          }
+      }  
+
+      //-----Manual close passenger door control-------------------------------------------------------------------------------
       else if (inData == "Manual passenger close\n"){
         on_press_passenger_close();
       }
+
+      //-----Manual passenger stop control-------------------------------------------------------------------------------------
       else if (inData == "Manual passenger stop\n"){
         on_release_passenger();
       }
+
+      //-----Invalid command---------------------------------------------------------------------------------------------------
       else {
         Serial.println("Please input valid command\n");
     }
@@ -108,7 +229,8 @@ void loop() {
     }
   }
   
-// Driver side switch function (Two switches, one itnernl and one external). See schematic
+// Driver side switch function (Two switches, one itnernal and one external). See schematic-------------------------------------
+// Driver switch press logic control-------------------------------------------------------------------------------------------
   int Driver_Switch_State = digitalRead(DriverSwitch);
   if (Driver_Switch_State == HIGH) {
     // the function here will depend on the current getFeedback from the actuator. If the motor actuator
@@ -120,13 +242,14 @@ void loop() {
     jrk1.setTarget(2400);
   }
   else {
-    Serial.println("Do Nothing");
+    //Serial.println("Do Nothing");
 //  uint8_t msg[] = {86, 85, 32, 74,85, 73,67, 69}; // Message feedback to phone
 //  SerialBT.write(msg, 8);
-//  delay(3000);
+  //delay(2000);
   }
 
-// Passenger side switch function (Two switches, one internal and one external). See schematic
+// Passenger side switch function (Two switches, one internal and one external). See schematic---------------------------------
+// Passenger switch press logic control----------------------------------------------------------------------------------------
   int Passenger_Switch_State = digitalRead(PassengerSwitch);
   if (Passenger_Switch_State == HIGH) {
     // the function here will depend on the current getFeedback from the actuator. If the motor actuator
@@ -138,9 +261,13 @@ void loop() {
     jrk2.setTarget(1750);
   }
   else {
-    Serial.println("Do Nothing");
+    //delay(2000);
+    //Serial.println("Do Nothing");
   }
 }
+
+
+//--------------------------------------------List of functions for main loop--------------------------------------------------
 
 void autoclose_driver(){
 jrk1.setTarget(2400);
